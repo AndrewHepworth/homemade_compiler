394b416b6769f977b34ee6cd6683189e2c58eb62
Andrew <andyhepworth95@gmail.com>
2025-03-15 13:59:01 +0000

    renamed main file

--- java/Lox.java
+++ java/Lox.java
@@ -1,5 +1,6 @@
-public class Main {
+public class Lox {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Faggot is here");
     }
 }
