dcb7a365fbda89710b354e11fa91de3215d3a439
Andrew <andyhepworth95@gmail.com>
2025-03-26 16:32:57 +0000

    created statement class that will allow us to parse in statements

--- java/tool/GenerateAST.java
+++ java/tool/GenerateAST.java
@@ -20,6 +20,11 @@ public class GenerateAST {
           "Literal    : Object value",
           "Unary      : Token operator, Expr right"
         ));
+
+        defineAst(outputDir, "Stmt", Arrays.asList(
+                "Expression  : Expr expression",
+                "Print       : Expr expression"
+        ));
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types) throws FileNotFoundException, UnsupportedEncodingException {
@@ -78,7 +83,6 @@ public class GenerateAST {
         }
         writer.println("   }");
     }
-
 
 }
 
